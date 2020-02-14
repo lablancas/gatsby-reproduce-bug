6cef7d9f610a9a42db0f5287efcad47a8c0e7375
Lucas Blancas <lablancas@gmail.com>
2020-02-13 22:09:18 -0600

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,53 @@
+const activeEnv = process.env.ACTIVE_ENV || process.env.NODE_ENV || 'development'
+
+console.log('[gatsby-config] ' + activeEnv)
+
+require('dotenv').config({
+  path: `${ __dirname }/.env.${ activeEnv }`,
+})
+
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: 'gatsby-source-graphql',
+      options: {
+        typeName: 'Lineups',
+        fieldName: 'lineups',
+        url: process.env.GRAPHQL_API_URI,
+        headers: {
+          Authorization: `Bearer ${ process.env.GRAPHQL_API_KEY }`,
+        },
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
